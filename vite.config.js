c3256efa9e68062ea4cccb8334fd418d21c92b1c
Mowlee-03 <mowleemm@gmail.com>
2025-08-07 16:32:39 +0530

    msg added

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/",
+  base: '/project1/web/',
   server: {
     host: '0.0.0.0', // 👈 this is required for Vagrant access
     port: 5173,      // optional: ensures consistent port
